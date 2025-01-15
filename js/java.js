7e7bc85f924e3c3af0286a19f093e553f88986ee
Hunter2454254 <126Hhaynes@frhsd.com>
2025-01-15 13:41:56 -0500

    h

--- js/java.js
+++ js/java.js
@@ -1,13 +1,21 @@
 
 const carImages = document.querySelectorAll('.car-image');
-
-
 const mainCarImage = document.getElementById('carImage');
-
 
 carImages.forEach(carImage => {
     carImage.addEventListener('mouseover', function() {
         const newCarSrc = `img/${carImage.getAttribute('data-car')}`;
         mainCarImage.src = newCarSrc;
     });
+});
+
+$(document).ready(function(){
+    $(".btn-show-image").hover(
+        function() {
+            $(this).css("background-color", "yellow");
+        }, 
+        function() {
+            $(this).css("background-color", "#4CAF50");
+        }
+    );
 });
